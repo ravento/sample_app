1355a753efd8c9083b905a19a4eb0477065c9465
raven <ravenhoto@gmail.com>
2013-06-14 17:57:30 +0800

    sign-up temp

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
-  match '/signup', to:'users#new'   
-  match '/help', to: 'static_pages#help' 
-  match '/about', to: 'static_pages#about' 
+	resources :users
+  match '/signup', to:'users#new'
+  match '/help', to: 'static_pages#help'
+  match '/about', to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
   root to: 'static_pages#home'
   # The priority is based upon order of creation:
