cf5c64a167771e0ff856464e167f49bd50c8737c
raven <ravenhoto@gmail.com>
2013-06-24 16:50:21 +0800

    Add user microposts

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,12 +9,26 @@ describe "Static pages" do
 
      describe "Home page" do
           before { visit root_path }
-          let(:heading) { 'Sample App' } 
+          let(:heading) { 'Sample App' }
           let(:page_title) { '' }
           it_should_behave_like "all static pages"
-          it { should have_selector('h1', text: 'Sample App') } 
-          it { should have_selector('title', text: full_title('')) } 
+          it { should have_selector('h1', text: 'Sample App') }
+          it { should have_selector('title', text: full_title('')) }
           it { should have_selector 'title', text: '| Home' }
+          describe "for signed-in users" do
+						let(:user) { FactoryGirl.create(:user) }
+						before do
+							FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+							FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+							sign_in_user
+							visit root_path
+						end
+						it "should render the user's feed" do
+							user.feed.each do |item|
+								page.should have_selector("li##{item.id}", text: item.content)
+							end
+						end
+					end
      end
 
      describe "Help page" do
@@ -22,14 +36,14 @@ describe "Static pages" do
           it { should have_selector('h1',text: 'Help') }
           it { should have_selector('title', text: full_title('Help')) }
      end
-     
-     describe "About page" do 
+
+     describe "About page" do
           before { visit about_path }
           it { should have_selector('h1',text: 'About') }
           it { should have_selector('title',text: full_title('About Us')) }
      end
-     
-     describe "Contact page" do 
+
+     describe "Contact page" do
           before { visit contact_path }
           it { should have_selector('h1',text: 'Contact') }
           it { should have_selector('title', text: full_title('Contact')) }
@@ -38,7 +52,7 @@ describe "Static pages" do
      it "should have the right links on the layout" do
           visit root_path
           click_link "About"
-          page.should have_selector 'title', text: full_title('About Us') 
+          page.should have_selector 'title', text: full_title('About Us')
           click_link "Help"
           page.should # fill in
           click_link "Contact"
