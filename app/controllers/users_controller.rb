f5fd832bff29182571aced2e703b22969a5765e8
raven <ravenhoto@gmail.com>
2013-07-29 16:09:41 +0800

    modify users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -51,10 +51,15 @@ class UsersController < ApplicationController
 	end
 
 	private
+		def signed_in_user
+			redirect_to signin_path, notice: "Please sign in." unless signed in?
+		end
+
 		def correct_user
 			@user = User.find(params[:id])
 			redirect_to(root_path) unless current_user?(@user)
 		end
+		
 		def admin_user
 			redirect_to(root_path) unless current_user.admin?
 		end
